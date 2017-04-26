5ec69e5d2e31b9e9a14e9cf4d490195af2c6fb89
suryasadhana <suryasadhana74@gmail.com>
2017-04-26 14:39:04 +0530

    Create prgm5.c

---
+++ prgm5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int year;
+printf("enter the year");
+scanf("%d",&year);
+if(year%4==0)
+{
+if(year%100==0)
+{
+if(year%400==0)
+{
+printf("leap year");
+}
+}
+}
+getch();
+}
